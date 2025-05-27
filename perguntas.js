38454ff26c4fcb7f7d5a851af298eb72ae3f63a3
camisanjosz <camila.araujo.anjos@escola.pr.gov.br>
2025-05-27 19:45:56 +0000

    terceira e segunda pergunta

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,17 @@ criaCartao(
     'Qual Jogador ficou conhecido como "Rei Do Futebol"?',
     'Pelé'
 )
+
+
+criaCartao(
+    'Futebol Brasileiro',
+    'Qual o maior estádio de futebol do Brasil?',
+    'Maracanã'
+)
+
+
+criaCartao(
+    'Futebol Brasileiro',
+    'Contra qual time o jogador Pelé marcou seu gol de número 1000?',
+    'Contra o Vasco da Gama'
+)
