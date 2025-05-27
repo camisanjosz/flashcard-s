7e9ddef763202cdbf5f5a6af83736f9d6076d278
camisanjosz <camila.araujo.anjos@escola.pr.gov.br>
2025-05-27 02:31:42 -0300

    Click
    Adicionado o sistema de click.

--- flashcard/app.js
+++ flashcard/app.js
@@ -21,7 +21,7 @@ function criaCartao(categoria, pergunta, resposta) {
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
     }
-    cartao.addEventListener('click', viraCartao);
+    cartao.addEventListener('click', viraCartao)
 
 
     container.appendChild(cartao)
