435cdebf66a480aea098028571fdbf27032fd7f0
camisanjosz <camila.araujo.anjos@escola.pr.gov.br>
2025-05-27 01:58:12 -0300

    Erro Arrumado
    o erro não estava no código em si, mas sim no console no "inspecionar" no navegador que eu estava usando. Agora, arrumando o console as perguntas estão todas visíveis no arquivo. Agora, finalizando o sistema do click

---
+++ flashcard/perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Futebol Brasileiro',
+    'Qual Jogador ficou conhecido como "Rei Do Futebol"?',
+    'Pelé'
+)
+
+criaCartao(
+    'Futebol Brasileiro',
+    'Qual o maior estádio de futebol do Brasil?',
+    'Maracanã'
+)
+
+criaCartao(
+    'Futebol Brasileiro',
+    'Contra qual time o jogador Pelé marcou seu gol de número 1000?',
+    'Contra o Vasco da Gama'
+)
+
+criaCartao(
+    'Futebol Brasileiro',
+    'Qual Jogador conhecido como "Fenômeno" no futebol brasileiro?',
+    'Ronaldo'
+)
+
+criaCartao(
+    'Jogadores',
+    'Qual jogador venceu a bola de ouro de 2012?',
+    'Lionel Messi'
+)
+
+criaCartao(
+    'Jogadores',
+    'Qual jogador venceu a bola de ouro de 1998?',
+    'Zinedine Zidane'
+)
+
+criaCartao(
+    'Jogadores',
+    'Quem é o jogador português conhecido como CR7?',
+    'Cristiano Ronaldo'
+)
+
+criaCartao(
+    'Jogadores',
+    'Quem é o jogador com mais gols em uma única edição de Copa do Mundo?',
+    'Just Fontaine'
+)
+
+criaCartao(
+    'Geral',
+    'Quais são os 7 fundamentos do futebol?',
+    'passe, condução de bola, chute, drible, domínio, recepção e cabeceio.'
+)
+
+criaCartao(
+    'Geral',
+    'Quais são as 11 posições do futebol?',
+    'goleiro, lateral direito, lateral esquerdo, zagueiro central, zagueiro central, volante (ou meia-defensivo), meia-central, meia-direito/ala, meia-esquerdo/ala e dois atacantes (um centroavante e um ponta).'
+)
+
+criaCartao(
+    'Geral',
+    'Em que ano foi realizada a primeira Copa do Mundo de futebol e em qual país?',
+    'no ano de 1930, no Uruguai'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o país de origem do futebol moderno?',
+    'Inglaterra.'
+)
