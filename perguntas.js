453d791bd8ccf2357201aa49e5af91a37a60f636
camisanjosz <camila.araujo.anjos@escola.pr.gov.br>
2025-05-27 19:46:22 +0000

    quarta e quinta

--- perguntas.js
+++ perguntas.js
@@ -17,3 +17,16 @@ criaCartao(
     'Contra qual time o jogador Pelé marcou seu gol de número 1000?',
     'Contra o Vasco da Gama'
 )
+
+criaCartao(
+    'Futebol Brasileiro',
+    'Qual Jogador conhecido como "Fenômeno" no futebol brasileiro?',
+    'Ronaldo'
+)
+
+
+criaCartao(
+    'Jogadores',
+    'Qual jogador venceu a bola de ouro de 2012?',
+    'Lionel Messi'
+)
